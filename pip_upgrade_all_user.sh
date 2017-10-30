38467c2fbde4d381d42328b1324807ca558acf13
Jeffrey Grover <groverj3@gmail.com>
2017-10-29 18:34:33 -0700

    Upgrade python 2 and 3 packages

--- pip_upgrade_all_user.sh
+++ pip_upgrade_all_user.sh
@@ -4,4 +4,5 @@
 # Created: 08/2017
 # Purpose: Update all python packages
 
-pip list --outdated | cut -d' ' -f1 | xargs pip install -U --user
+pip list --outdated | cut -d' ' -f1 | xargs pip install -U --user && \
+pip3 list --outdated | cut -d' ' -f1 | xargs pip3 install -U --user

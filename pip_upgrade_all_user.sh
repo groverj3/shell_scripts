0b957ad05c9a02b6f38a96ae6d317fd0628abc2f
groverj3 <groverj3@gmail.com>
2019-03-05 01:19:16 -0700

    Better method for this

--- pip_upgrade_all_user.sh
+++ pip_upgrade_all_user.sh
@@ -4,5 +4,5 @@
 # Created: 08/2017
 # Purpose: Update all --user installed python packages
 
-pip2 list -v | grep 'home' | grep 'pip' | cut -d ' ' -f 1 | xargs pip2 install -U --user \
+pip2 list -v | grep 'home' | grep 'pip' | cut -d ' ' -f 1 | xargs pip2 install -U --user
 pip3 list -v | grep 'home' | grep 'pip' | cut -d ' ' -f 1 | xargs pip3 install -U --user

cbd9f409c7ebfffae7277bc200d20ddfae9af73d
Jeffrey Grover <groverj3@gmail.com>
2020-04-16 14:39:37 -0700

    Delete pip_upgrade_all_user.sh
    pip2 and pip3 unnecessary since deprecation of python 2

--- pip_upgrade_all_user.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Author: Jeffrey Grover
-# Created: 08/2017
-# Purpose: Update all --user installed python packages
-
-pip2 list --outdated -v | grep 'home' | cut -d ' ' -f 1 | xargs pip2 install -U --user
-pip3 list --outdated -v | grep 'home' | cut -d ' ' -f 1 | xargs pip3 install -U --user

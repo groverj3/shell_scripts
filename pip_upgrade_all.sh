72230a2c810a53c499fecb2dabf4c9ab93a43e0b
Jeffrey Grover <jeffrey.w.grover@gmail.com>
2022-05-22 00:39:51 -0400

    Updated my lonely script in here

--- pip_upgrade_all.sh
+++ pip_upgrade_all.sh
@@ -5,4 +5,4 @@
 # Purpose: Update all installed python packages, best if used with pyenv
 # Alternatively, put something like this in .bashrc as an alias
 
-pip list --outdated | tail -n +3 | cut -d ' ' -f 1 | xargs pip install -U
+pip install -U pip && pip list --outdated | tail -n +3 | cut -d ' ' -f 1 | xargs pip install -U
